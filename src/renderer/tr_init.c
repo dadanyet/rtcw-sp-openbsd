3c0e2bbefe9593d87a25b9a442eb2270fdb9288e
Joshua Kahlenberg <jkahl@THOUGHTCRIME.invalid>
2015-01-04 15:12:15 -0600

    fix crash by printing two gl extensions/line. from knightmare's diff

--- src/renderer/tr_init.c
+++ src/renderer/tr_init.c
@@ -844,7 +844,25 @@ void GfxInfo_f( void ) {
 	ri.Printf( PRINT_ALL, "\nGL_VENDOR: %s\n", glConfig.vendor_string );
 	ri.Printf( PRINT_ALL, "GL_RENDERER: %s\n", glConfig.renderer_string );
 	ri.Printf( PRINT_ALL, "GL_VERSION: %s\n", glConfig.version_string );
-	ri.Printf( PRINT_ALL, "GL_EXTENSIONS: %s\n", glConfig.extensions_string );
+	{
+		char *extString, *extTok;
+		unsigned line = 0;
+		ri.Printf( PRINT_ALL, "GL_EXTENSIONS: " );
+		extString = (char *)glConfig.extensions_string;
+		while (1)
+		{
+			extTok = COM_Parse(&extString);
+			if (!extTok[0])
+				break;
+			line++;
+			if ((line % 2) == 0)
+				ri.Printf ( PRINT_ALL, "%s\n", extTok );
+			else
+				ri.Printf ( PRINT_ALL, "%s ", extTok );
+		}
+		if ((line % 2) != 0)
+			ri.Printf ( PRINT_ALL, "\n" );
+	}
 	ri.Printf( PRINT_ALL, "GL_MAX_TEXTURE_SIZE: %d\n", glConfig.maxTextureSize );
 	ri.Printf( PRINT_ALL, "GL_MAX_ACTIVE_TEXTURES_ARB: %d\n", glConfig.maxActiveTextures );
 	ri.Printf( PRINT_ALL, "\nPIXELFORMAT: color(%d-bits) Z(%d-bit) stencil(%d-bits)\n", glConfig.colorBits, glConfig.depthBits, glConfig.stencilBits );
